c3758b3fc53f5d492019a7c52c1731c3f80f2ac6
Muskan <167499932+mu243@users.noreply.github.com>
2024-08-21 02:01:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import envCopatible from "vite-plugin-env-compatible";
-// https://vitejs.dev/config/
+
 export default defineConfig({
-  // envPrefix: "VITE_",
+  server: {
+    port: process.env.PORT || 3000, // Ensure the app uses the PORT environment variable or defaults to 3000
+  },
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 500, // Keep this warning limit, or you can increase it if necessary
+  },
 });
