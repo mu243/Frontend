845492877292c4fe8ebc920a52c0e9bd23b31502
mu243 <167499932+mu243@users.noreply.github.com>
2024-08-21 10:58:22 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: process.env.PORT || 3000, // Ensure the app uses the PORT environment variable or defaults to 3000
-  },
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        },
-      },
-    },
-    chunkSizeWarningLimit: 500, // Keep this warning limit, or you can increase it if necessary
-  },
-});
+})
